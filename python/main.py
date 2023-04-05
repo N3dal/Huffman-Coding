ed8825e345880fc3048a2844547f1b4176f24df4
N3dal <nedalxzo@gmail.com>
2023-04-05 05:00:37 +0200

    create find_max_binary_source_entropy

--- python/main.py
+++ python/main.py
@@ -38,10 +38,33 @@ def find_binary_source_entropy(message: str):
     return sum((symbol_probabilities[char] * math.log2(1/symbol_probabilities[char])) for char in set(message))
 
 
+def find_max_binary_source_entropy(message: str):
+    """
+        :ARGS:
+            message:str => the message we want to find the max binary source entropy;
+
+        :RETURNS:
+            return int;
+
+        :INFO:
+            find the max binary entropy for the given message;
+    """
+
+    symbol_count = len(message)
+
+    # approximate the symbol count to the greater nearest 2^n;
+
+    while math.log2(symbol_count) % 1 > 0:
+        symbol_count += 1
+
+    return int(math.log2(symbol_count))
+
+
 def main():
 
     # print(find_binary_source_entropy("AAAAABBCCD"))
     print(find_binary_source_entropy(TEST_MESSAGE))
+    print(find_max_binary_source_entropy(TEST_MESSAGE))
 
 
 if __name__ == "__main__":
