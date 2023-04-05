3e905586a321f6f299811babf37f4b611b8f883f
N3dal <nedalxzo@gmail.com>
2023-04-05 04:52:17 +0200

    created find_binary_source_entropy

--- python/main.py
+++ python/main.py
@@ -15,8 +15,33 @@
 from utils import *
 
 
+def find_binary_source_entropy(message: str):
+    """
+        :ARGS:
+            message:str => the message we want to find its binary entropy.
+
+        :RETURNS:
+            return float, "bit/symbol";
+
+        :INFO:
+            we define this as a Logarithmic scale for data transfer in rate in a message;
+            and its the minimum amount of bits that exist in a symbol;
+    """
+
+    assert isinstance(
+        message, str), f"message must be a str type not {type(message)}"
+
+    symbol_count = len(message)
+
+    symbol_probabilities = message_chars_probability(message)
+
+    return sum((symbol_probabilities[char] * math.log2(1/symbol_probabilities[char])) for char in set(message))
+
+
 def main():
-    pass
+
+    # print(find_binary_source_entropy("AAAAABBCCD"))
+    print(find_binary_source_entropy(TEST_MESSAGE))
 
 
 if __name__ == "__main__":
